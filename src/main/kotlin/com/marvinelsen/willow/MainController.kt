c4efe85eca7c4167a6b9fefb039cb5bdee5a9639
Marvin Elsen <git@marvinelsen.com>
2023-11-05 17:17:17 +0100

    Implement word display screen

--- src/main/kotlin/com/marvinelsen/willow/MainController.kt
+++ src/main/kotlin/com/marvinelsen/willow/MainController.kt
@@ -6,24 +6,36 @@ import com.marvinelsen.willow.ui.cells.WordCellFactory
 import javafx.collections.FXCollections
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.scene.control.Label
 import javafx.scene.control.ListView
 import javafx.scene.control.TextField
+import javafx.scene.control.TitledPane
 import javafx.scene.input.ContextMenuEvent
-import org.jetbrains.exposed.sql.transactions.transaction
+import javafx.scene.paint.Color
+import javafx.scene.text.Text
+import javafx.scene.text.TextFlow
+import javafx.scene.web.WebView
 
 class MainController {
-    @FXML
+    lateinit var webViewCedict: WebView
+    lateinit var titledPaneCedict: TitledPane
+    lateinit var webViewMoe: WebView
+    lateinit var titledPaneMoe: TitledPane
+    lateinit var webViewUser: WebView
+    lateinit var titledPaneUser: TitledPane
+    lateinit var labelHeadwordPronunciation: Label
+    lateinit var textFlowHeadWord: TextFlow
     lateinit var textFieldSearch: TextField
-
-    @FXML
     lateinit var listViewDictionary: ListView<CedictEntity>
 
-    @FXML
     fun initialize() {
         listViewDictionary.cellFactory = WordCellFactory()
 
         listViewDictionary.items = FXCollections.observableArrayList()
         listViewDictionary.items.addAll(CedictService.search("柳"))
+        listViewDictionary.selectionModel
+            .selectedItemProperty()
+            .addListener { observableValue, entry, newEntry -> displayWord(newEntry) }
         listViewDictionary.selectionModel.selectFirst()
 
         textFieldSearch.textProperty().addListener { observable, oldValue, newValue ->
@@ -48,7 +60,28 @@ class MainController {
         listViewDictionary.items.addAll(CedictService.search(textFieldSearch.text))
     }
 
-    private fun search(query: String) = FXCollections.observableList(transaction {
-        CedictEntity.find { CedictTable.traditional like "$query%" }.toList()
-    })
+    private fun displayWord(word: CedictEntity) {
+        textFlowHeadWord.children.clear()
+        webViewCedict.engine.loadContent("")
+        titledPaneCedict.isVisible = false
+        titledPaneUser.isManaged = false
+        webViewUser.engine.loadContent("")
+        titledPaneUser.isVisible = false
+        titledPaneUser.isManaged = false
+        webViewMoe.engine.loadContent("")
+        titledPaneMoe.isVisible = false
+        titledPaneMoe.isManaged = false
+        val characters = word.traditional.split("")
+        for (i in characters.indices) {
+            val characterText = Text(characters[i])
+            characterText.fill = Color.web("#000")
+            characterText.styleClass.add("headword")
+            textFlowHeadWord.children.add(characterText)
+        }
+        labelHeadwordPronunciation.text = word.numberedPinyin
+        webViewCedict.engine.loadContent(word.definitions.joinToString(separator = "<br>- "))
+        titledPaneCedict.isVisible = true
+        titledPaneCedict.isManaged = true
+
+    }
 }
