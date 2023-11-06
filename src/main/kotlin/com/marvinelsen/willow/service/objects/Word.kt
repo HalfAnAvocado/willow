505def748ea8ab632cef2281f3a5e2f4d3991210
Marvin Elsen <git@marvinelsen.com>
2023-11-06 22:26:12 +0100

    Reformat

--- src/main/kotlin/com/marvinelsen/willow/service/objects/Word.kt
+++ src/main/kotlin/com/marvinelsen/willow/service/objects/Word.kt
@@ -2,11 +2,13 @@ package com.marvinelsen.willow.service.objects
 
 import com.marvinelsen.willow.persistence.entities.WordEntity
 
+data class Word(
+    val traditional: String,
+    val simplified: String?,
+    val definitions: Map<Dictionary, List<Definition>>,
+)
 
-data class Word(val traditional: String, val simplified: String?, val definitions: Map<Dictionary, List<Definition>>)
-
-fun WordEntity.asWord() =
-    Word(
-        traditional = traditional,
-        simplified = simplified,
-        definitions = definitions.map { it.asDefinition() }.groupBy { it.dictionary })
+fun WordEntity.asWord() = Word(
+    traditional = traditional,
+    simplified = simplified,
+    definitions = definitions.map { it.asDefinition() }.groupBy { it.dictionary })
