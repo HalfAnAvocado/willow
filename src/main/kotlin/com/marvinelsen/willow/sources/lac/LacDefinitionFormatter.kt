bdb3b0a5bcc58b36db1da2003fdec94c427dd79a
Marvin Elsen <git@marvinelsen.com>
2023-12-04 20:12:47 +0100

    Refactor LAC definition to use KotlinX HTML

--- src/main/kotlin/com/marvinelsen/willow/sources/lac/LacDefinitionFormatter.kt
+++ src/main/kotlin/com/marvinelsen/willow/sources/lac/LacDefinitionFormatter.kt
@@ -1,46 +1,50 @@
 package com.marvinelsen.willow.sources.lac
 
 import com.marvinelsen.willow.sources.common.DefinitionFormatter
+import kotlinx.html.div
+import kotlinx.html.li
+import kotlinx.html.ol
+import kotlinx.html.span
+import kotlinx.html.stream.createHTML
 
 object LacDefinitionFormatter : DefinitionFormatter<LacEntry> {
-    override fun formatHtmlDefinition(entry: LacEntry) = buildString {
-        if (entry.definitions.size == 1) {
-            append(formatDefinition(entry.definitions.first(), entry.traditional))
-        } else {
-            append("<ol>")
+    override fun formatHtmlDefinition(entry: LacEntry) = createHTML().div {
+        ol {
             entry.definitions.forEach { definition ->
-                append("<li>")
-                append(formatDefinition(definition, entry.traditional))
-                append("</li>")
+                li {
+                    span(classes = "definition") {
+                        +definition.sanitizeDefinition()
+                    }
+                    if ("[例]" in definition) {
+                        val examples = definition
+                            .substringAfter("[例]")
+                            .sanitizeExamplesFor(entry.traditional)
+                            .split("｜")
+
+                        span(classes = "example") {
+                            +examples.joinToString(
+                                prefix = "如：",
+                                separator = "、",
+                                postfix = "。"
+                            ) { "「$it」" }
+                        }
+                    }
+                }
             }
-            append("</ol>")
         }
     }
 
-    override fun formatShortDefinition(entry: LacEntry) =
-        entry.definitions.first()
-            .substringAfter('.')
-            .substringBefore("[例]")
-            .replace("★", "【大陸】")
-            .replace("▲", "【臺灣】")
+    override fun formatShortDefinition(entry: LacEntry) = entry.definitions.first().sanitizeDefinition()
 
-    private fun formatDefinition(definition: String, traditional: String) = buildString {
-        val formattedDefinition = definition.substringAfter('.')
-            .replace("～", "<span class=\"headword\">${traditional}</span>")
-            .replace("★", "【大陸】")
-            .replace("▲", "【臺灣】")
+    private fun String.sanitizeDefinition() = this
+        .substringAfter('.')
+        .substringBefore("[例]")
+        .replace("★", "【大陸】")
+        .replace("▲", "【臺灣】")
 
-        val examples = if (formattedDefinition.indexOf("[例]") > -1) formattedDefinition.substringAfter("[例]")
-            .replace("。", "")
-            .split("｜") else null
-
-        append("<span class=\"definition\">${formattedDefinition.substringBefore("[例]")}</span>")
-        if (examples != null) {
-            append(examples.joinToString(
-                prefix = "<span class=\"example\">如：",
-                separator = "、",
-                postfix = "。</span>"
-            ) { "「$it」" })
-        }
-    }
+    private fun String.sanitizeExamplesFor(traditional: String) = this
+        .replace("～", traditional)
+        .replace("★", "【大陸】")
+        .replace("▲", "【臺灣】")
+        .replace("。", "")
 }
