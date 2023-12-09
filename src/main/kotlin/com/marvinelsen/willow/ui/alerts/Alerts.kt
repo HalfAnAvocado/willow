33c6989400a08c6b0b803e8b2ff30480a8be2ef7
Marvin Elsen <git@marvinelsen.com>
2023-12-09 22:52:26 +0100

    Remove information alerts for creating new entities

--- src/main/kotlin/com/marvinelsen/willow/ui/alerts/Alerts.kt
+++ src/main/kotlin/com/marvinelsen/willow/ui/alerts/Alerts.kt
@@ -2,19 +2,8 @@ package com.marvinelsen.willow.ui.alerts
 
 import javafx.scene.control.Alert
 
-val addedUserSentenceAlert = Alert(Alert.AlertType.INFORMATION).apply {
-    title = "Successfully added sentence"
-    headerText = "Successfully added new example sentence"
-    contentText = ""
-    isResizable = false
-}
-
-val addedUserEntryAlert = Alert(Alert.AlertType.INFORMATION).apply {
-    title = "Successfully added entry"
-    headerText = "Successfully added new entry"
-    contentText = ""
-    isResizable = false
-}
+@DslMarker
+annotation class AlertDsl
 
 @AlertDsl
 fun alert(type: Alert.AlertType, init: Alert.() -> Unit): Alert {
@@ -22,6 +11,3 @@ fun alert(type: Alert.AlertType, init: Alert.() -> Unit): Alert {
     alert.init()
     return alert
 }
-
-@DslMarker
-annotation class AlertDsl
