488c30f76bb2fa66817e277b4e72af6a25c6a97e
Marvin Elsen <git@marvinelsen.com>
2023-12-09 22:32:42 +0100

    Refactor New Sentence dialog

--- src/main/kotlin/com/marvinelsen/willow/ui/dialogs/NewSentenceDialog.kt
+++ src/main/kotlin/com/marvinelsen/willow/ui/dialogs/NewSentenceDialog.kt
@@ -2,19 +2,26 @@ package com.marvinelsen.willow.ui.dialogs
 
 import com.marvinelsen.willow.WillowApplication
 import com.marvinelsen.willow.dictionary.Sentence
+import com.marvinelsen.willow.ui.alerts.alert
+import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
+import javafx.scene.control.Alert
 import javafx.scene.control.ButtonType
 import javafx.scene.control.Dialog
+import javafx.scene.control.DialogPane
 import javafx.scene.control.TextArea
 import javafx.scene.input.Clipboard
-import javafx.scene.layout.GridPane
 import javafx.stage.Modality
 import javafx.stage.Stage
 import javafx.stage.Window
 import javafx.util.Callback
 
-class NewSentenceDialog(owner: Window?, exampleSentence: String?) : Dialog<Sentence?>() {
+class NewSentenceDialog(owner: Window?) : Dialog<Sentence?>() {
+    @Suppress("MemberVisibilityCanBePrivate")
     lateinit var textAreaChineseSentence: TextArea
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    lateinit var buttonTypeOk: ButtonType
 
     private val systemClipboard = Clipboard.getSystemClipboard()
 
@@ -22,27 +29,50 @@ class NewSentenceDialog(owner: Window?, exampleSentence: String?) : Dialog<Sente
         val loader = FXMLLoader(WillowApplication::class.java.getResource("views/new-sentence-dialog.fxml"))
         loader.setController(this)
 
-        val root: GridPane = loader.load()
+        val root: DialogPane = loader.load()
 
-        textAreaChineseSentence.text = exampleSentence ?: ""
+        dialogPane = root
+        title = "New Sentence"
+        isResizable = true
 
         initOwner(owner)
         initModality(Modality.APPLICATION_MODAL)
 
-        title = "Add Sentence"
-        headerText = "Add an example sentence to the dictionary..."
-        isResizable = true
+        (dialogPane.scene.window as Stage).apply {
+            minWidth = 400.0
+            minHeight = 250.0
+        }
 
-        dialogPane.content = root
-        dialogPane.buttonTypes.addAll(ButtonType.CLOSE, ButtonType.OK)
+        val buttonOk = root.lookupButton(buttonTypeOk)
+        buttonOk.addEventFilter(ActionEvent.ACTION, ::validateUserInput)
+        buttonOk.disableProperty().bind(textAreaChineseSentence.textProperty().isEmpty)
 
-        val dialogStage = dialogPane.scene.window as Stage
-        dialogStage.minWidth = 380.0
-        dialogStage.minHeight = 250.0
-
-        resultConverter = Callback { buttonType: ButtonType -> returnResult(buttonType) }
+        resultConverter = Callback(::convertToResult)
     }
 
-    private fun returnResult(buttonType: ButtonType) =
-        if (ButtonType.OK == buttonType) Sentence(textAreaChineseSentence.text) else null
+    private fun validateUserInput(event: ActionEvent) {
+        when {
+            textAreaChineseSentence.text.isBlank() -> alert(Alert.AlertType.ERROR) {
+                title = "Invalid Input"
+                headerText = "Chinese sentence was blank"
+                contentText =
+                    "Chinese sentence was left blank, but should contain at least one Traditional Chinese character."
+            }.show()
+
+            else -> return
+        }
+        event.consume()
+    }
+
+    private fun convertToResult(buttonType: ButtonType) =
+        when (buttonType) {
+            ButtonType.OK -> Sentence(textAreaChineseSentence.text)
+
+            else -> null
+        }
+
+    @Suppress("Unused")
+    fun onButtonPasteIntoChineseSentenceAction() {
+        textAreaChineseSentence.text = systemClipboard.string
+    }
 }
