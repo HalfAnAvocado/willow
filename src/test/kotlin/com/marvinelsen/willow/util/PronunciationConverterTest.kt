4e0304b609e58f7a7144036b5a54efbba49ac28c
Marvin Elsen <git@marvinelsen.com>
2023-12-23 20:23:39 +0100

    Add Zhuyin to Accented Pinyin conversion

--- src/test/kotlin/com/marvinelsen/willow/util/PronunciationConverterTest.kt
+++ src/test/kotlin/com/marvinelsen/willow/util/PronunciationConverterTest.kt
@@ -31,4 +31,36 @@ class PronunciationConverterTest : FunSpec({
             PronunciationConverter.convertToNumberedPinyin(zhuyin) shouldBe numberedPinyin
         }
     }
+
+    context("Zhuyin to Accented Pinyin") {
+        withData(
+            TestCase("ㄧ", "yi\u0304"),
+            TestCase("ㄧˊ", "yi\u0301"),
+            TestCase("ㄧˇ", "yi\u030C"),
+            TestCase("ㄧˋ", "yi\u0300"),
+            TestCase("˙ㄧ", "yi"),
+            TestCase("ㄋㄩˇ", "nü\u030C"),
+            TestCase("ㄋㄩㄝˇ", "nüe\u030C"),
+            TestCase("ㄌㄩˇ", "lü\u030C"),
+            TestCase("ㄌㄩㄝˇ", "lüe\u030C"),
+            TestCase("ㄌㄩㄣˇ", "lü\u030Cn"),
+            TestCase("ㄞˋ", "a\u0300i"),
+            TestCase("ㄠˋ", "a\u0300o"),
+            TestCase("ㄟˋ", "e\u0300i"),
+            TestCase("ㄐㄧㄚˋ", "jia\u0300"),
+            TestCase("ㄐㄧㄠˋ", "jia\u0300o"),
+            TestCase("ㄐㄧㄝˋ", "jie\u0300"),
+            TestCase("ㄐㄩㄥˋ", "jio\u0300ng"),
+            TestCase("ㄐㄧㄡˋ", "jiu\u0300"),
+            TestCase("ㄔㄡˋ", "cho\u0300u"),
+            TestCase("ㄔㄨㄚˋ", "chua\u0300"),
+            TestCase("ㄔㄨㄞˋ", "chua\u0300i"),
+            TestCase("ㄐㄩㄝˋ", "jue\u0300"),
+            TestCase("ㄔㄨㄟˋ", "chui\u0300"),
+            TestCase("ㄔㄨㄛˋ", "chuo\u0300"),
+            TestCase("ㄌㄩㄝˋ", "lüe\u0300"),
+        ) { (zhuyin, accentedPinyin) ->
+            PronunciationConverter.convertToAccentedPinyin(zhuyin) shouldBe accentedPinyin
+        }
+    }
 })
